3e0bb07f81558995ef1b11e0075a7a8784987e7e
Gilbers, C. (Carolien) <caroliengilbers@gmail.com>
2020-08-03 14:25:35 +0200

    added separate controls for creature list item and attribute table

---
+++ CombatHelper/CombatHelper/Controls/EncounterCreatureListItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CombatHelper.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EncounterCreatureListItem : ContentView
+    {
+        public event EventHandler OnHealthClicked;
+        public static readonly BindableProperty OnHealthClickedProperty = BindableProperty.Create("OnHealthClicked", typeof(EventHandler), typeof(EncounterCreatureListItem));
+
+        public EncounterCreatureListItem()
+        {
+            InitializeComponent();
+        }
+
+        private void HealthButton_Clicked(object sender, EventArgs e)
+        {
+            OnHealthClicked.Invoke(sender, e);
+        }
+    }
+}
