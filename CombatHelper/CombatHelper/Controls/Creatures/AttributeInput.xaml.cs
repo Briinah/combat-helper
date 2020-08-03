013ea3cce4187ab6a41593a6a93ff2fa90068ec2
Gilbers, C. (Carolien) <caroliengilbers@gmail.com>
2020-08-03 15:21:34 +0200

    added custom header control

---
+++ CombatHelper/CombatHelper/Controls/Creatures/AttributeInput.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CombatHelper.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AttributeInput : ContentView
+    {
+        public event EventHandler OnNextEntry;
+        public static readonly BindableProperty OnNextEntryProperty = BindableProperty.Create("OnNextEntry", typeof(EventHandler), typeof(AttributeInput));
+
+        public AttributeInput()
+        {
+            InitializeComponent();
+        }
+
+        private void NextEntry_Completed(object sender, EventArgs e)
+        {
+            OnNextEntry.Invoke(sender, e);
+        }
+    }
+}
