d73c17530243dafe0e80bbfa5f89103cd6082d09
Gilbers, C. (Carolien) <caroliengilbers@gmail.com>
2021-01-09 16:10:00 +0100

    added ffimageloading

--- CombatHelper/CombatHelper.Android/MainActivity.cs
+++ CombatHelper/CombatHelper.Android/MainActivity.cs
@@ -19,9 +19,11 @@ namespace CombatHelper.Droid
 
             base.OnCreate(savedInstanceState);
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
             Xamarin.Forms.Forms.SetFlags("RadioButton_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
